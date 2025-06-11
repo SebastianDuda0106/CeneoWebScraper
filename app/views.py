25d2c7ce21c894fd6b50dbdd494775883e77fec1
Sebastian Duda <sebastian777duda@outlook.com>
2025-06-11 11:56:48 +0200

    2025-06-11 SD

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from app import app
 
-import requests,os,json
+import requests,os,json,csv
 import pandas as pd
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,send_file
 from bs4 import BeautifulSoup
 from matplotlib import pyplot as plt
 from app.config import headers
@@ -22,7 +22,6 @@ def display_form():
 @app.route("/extract", methods=["POST"])
 def extract(): 
     product_id = request.form.get('product_id')
-    
     next_page=f"https://www.ceneo.pl/{product_id}#tab=reviews"
     response = requests.get(next_page, headers=headers)
     if response.status_code ==200:
@@ -62,6 +61,17 @@ def extract():
             os.mkdir("./app/data/opinions")
         with open(f"./app/data/opinions/{product_id}.json",'w',encoding="UTF8") as jf:
             json.dump(all_opinions, jf, indent=4,ensure_ascii=False)
+
+    if not os.path.exists(f"./app/data/opinions/{product_id}.csv"):
+        with open(f"./app/data/opinions/{product_id}.json",'r',encoding="UTF8") as jf:
+            df=pd.read_json(jf)
+
+        with open(f"./app/data/opinions/{product_id}.csv",'w',encoding="UTF8") as csf:
+            df.to_csv(csf)
+    
+    if not os.path.exists(f"./app/data/opinions/{product_id}.xlsx"):
+        pd.read_json(f"./app/data/opinions/{product_id}.json",encoding="UTF8").to_excel(f"./app/data/opinions/{product_id}.xlsx")
+        
     else:
         opinions_exists=True
     if not os.path.isfile(f"./app/data/products/{product_id}_short.json"):
@@ -104,9 +114,9 @@ def extract():
             os.mkdir("./app/data/products")
         with open(f"./app/data/products/{product_id}_short.json",'w',encoding="UTF8") as jf:
             json.dump(stats, jf, indent=4,ensure_ascii=False)
-    
+        
     return redirect(url_for('product', product_id=product_id,product_name=product_name))
-
+    
 @app.route("/products")
 def products():
     products_files=os.listdir('app/data/products/')
@@ -162,6 +172,34 @@ def charts(product_id):
     plt.savefig(f"app/static/images/charts/{stats['product_id']}_pie.png")
     plt.close()
     return render_template("charts.html", product_id=product_id,product_name=stats['product_name'])
+
+@app.route("/charts2/<product_id>")
+def charts2(product_id):
+    if not os.path.exists("app/static/images"):
+        os.mkdir("app/static/images")
+    if not os.path.exists("app/static/images/charts"):
+        os.mkdir("app/static/images/charts")
+    
+    with open(f"app/data/products/{product_id}_short.json","r", encoding="UTF-8") as jf:
+        stats= json.load(jf)
+    recommendations = pd.Series(stats["recommendations"])
+    recommendations.plot.pie(
+        label="",
+        title="Rozkład rekomendacji w opiniach o produkcie",
+        labels=['Nie polecam','Polecam',"Nie mam zdania"],
+        colors=["crimson","forestgreen","lightgray"],
+        autopct="%1.1f%%"
+    )
+    plt.savefig(f"app/static/images/charts/{stats['product_id']}_pie.png")
+    plt.close()
+
+    categories = ['Nie polecam','Polecam','Nie mam zdania']
+    plt.bar(categories,recommendations.values)
+    plt.savefig(f"app/static/images/charts/{stats['product_id']}_bar.png")
+    plt.close
+
+    return render_template("charts2.html", product_id=product_id,product_name=stats['product_name'])
+
 @app.route("/author")
 def author():
     return render_template('author.html')
@@ -171,3 +209,25 @@ def product(product_id):
     product_name=request.args.get('product_name')
     opinions = pd.read_json(f"app/data/opinions/{product_id}.json")
     return render_template('product.html',product_id=product_id,product_name=product_name,opinions = opinions.to_html(table_id='opinions'))
+
+@app.route("/download_json/<product_id>")
+def download_json(product_id): 
+    return send_file(f'./data/opinions/{product_id}.json', as_attachment=True)
+
+@app.route("/download_csv/<product_id>")
+def download_csv(product_id):
+    if not os.path.exists(f"./app/data/opinions/{product_id}.csv"):
+        with open(f"./app/data/opinions/{product_id}.json",'r',encoding="UTF8") as jf:
+            df=pd.read_json(jf)
+
+        with open(f"./app/data/opinions/{product_id}.csv",'w',encoding="UTF8") as csf:
+            df.to_csv(csf)
+    
+    return send_file(f'./data/opinions/{product_id}.csv', as_attachment=True)
+
+@app.route("/download_xlsx/<product_id>")
+def download_xlsx(product_id): 
+    if not os.path.exists(f"./app/data/opinions/{product_id}.xlsx"):
+        pd.read_json(f"./app/data/opinions/{product_id}.json",encoding="UTF8").to_excel(f"./app/data/opinions/{product_id}.xlsx")
+        
+    return send_file(f'./data/opinions/{product_id}.xlsx', as_attachment=True)
